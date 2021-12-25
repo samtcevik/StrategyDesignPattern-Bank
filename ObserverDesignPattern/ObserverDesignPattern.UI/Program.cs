dfedb6344f4fb3de7013958f1d875dbb483a68ab
Samet ÇEVİK <samtcevik@gmail.com>
2021-12-25 03:46:06 +0300

    v1

---
+++ ObserverDesignPattern/ObserverDesignPattern.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverDesignPattern.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
